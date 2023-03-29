64b3279e05518799fc71816417208d35086096ee
leetxt13 <leetxt13@naver.com>
2023-03-29 13:24:20 +0900

    JS NAVBAR

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+//make navbar transparent or be pink as the scroll postion
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
